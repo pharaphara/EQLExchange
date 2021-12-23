21266999da49c6b75470e919e5018c8e6feeb5bc
Alexandre DELTOUR <alexandre.deltour@geodis.com>
2021-12-23 14:11:00 +0100

    modification

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/EQLExchangeApplication.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/EQLExchangeApplication.java
@@ -2,13 +2,20 @@ package fr.eql.al36.spring.projet.eqlexchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class EQLExchangeApplication extends SpringBootServletInitializer {
+public class EQLExchangeApplication extends SpringBootServletInitializer  {
 
     public static void main(String[] args) {
         SpringApplication.run(EQLExchangeApplication.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    	return application.sources(EQLExchangeApplication.class);
     }
 
 }
