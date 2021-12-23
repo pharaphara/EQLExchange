b8e223711695362b4c715109a927aff6989fed6d
Alexandre DELTOUR <alexandre.deltour@geodis.com>
2021-12-23 13:58:28 +0100

    mofif tomcat

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/EQLExchangeApplication.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/EQLExchangeApplication.java
@@ -2,9 +2,10 @@ package fr.eql.al36.spring.projet.eqlexchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EQLExchangeApplication {
+public class EQLExchangeApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EQLExchangeApplication.class, args);
