8a5632eb59d8d35148718e34966a6ce5e41b9773
Msaddek <basem.msaddek@gmail.com>
2022-01-06 00:25:03 +0100

    remove cors security

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/EQLExchangeApplication.java
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/EQLExchangeApplication.java
@@ -5,9 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EQLExchangeApplication extends SpringBootServletInitializer  {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedHeaders("*").allowedOrigins("http://localhost:4200").allowCredentials(true);
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EQLExchangeApplication.class, args);
