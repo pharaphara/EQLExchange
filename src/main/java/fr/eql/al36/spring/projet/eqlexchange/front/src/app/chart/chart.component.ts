8ae7f134668b214e60d67a71476755fbcdbda432
Msaddek <basem.msaddek@gmail.com>
2022-01-16 14:40:40 +0100

    update dashboard

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/front/src/app/chart/chart.component.ts
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/front/src/app/chart/chart.component.ts
@@ -1,5 +1,7 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
-import { createChart } from 'lightweight-charts';
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
+import {createChart} from 'lightweight-charts';
+import {from, map, Observable} from "rxjs";
+import * as io from 'socket.io-client';
 
 @Component({
   selector: 'app-chart',
@@ -8,50 +10,72 @@ import { createChart } from 'lightweight-charts';
 })
 export class ChartComponent implements OnInit, AfterViewInit {
 
-  private symbolPair: string = 'BNB/USDT';
-  public data$: Promise<any> = fetch(`https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m&limit=1000`);
+  private symbolPair: string = 'BNBUSDT';
 
-  constructor() { }
+  public data$: Promise<any> = fetch(`https://api.binance.com/api/v3/klines?symbol=${this.symbolPair}&interval=1m&limit=1000`);
 
-  ngAfterViewInit(): void {
-      this.dochart()
-    }
-
-  ngOnInit(): void {
-
+  constructor() {
   }
 
-  dochart(){
+  ngAfterViewInit(): void {
+    this.dochart()
+  }
+
+  ngOnInit(): void {}
+
+
+  price: number | undefined
+
+  dochart() {
 
     const log = console.log;
 
     const chartProperties = {
-      width:1500,
-      height:600,
+      width: 1300,
+      height: 600,
       layout: {
         backgroundColor: '#0b1217',
-        lineColor: '#2B2B43',
+        lineColor: '#0dcaf0',
         textColor: '#D9D9D9',
       },
-      timeScale:{
-        timeVisible:true,
-        secondsVisible:false,
+      grid: {
+        vertLines: {
+          color: '#142028',
+        },
+        horzLines: {
+          color: '#142028'
+        }
+      },
+      timeScale: {
+        timeVisible: true,
+        secondsVisible: false,
       }
     }
 
-    const domElement = document.getElementById('tvchart')  || '';
-    const chart = createChart(domElement,chartProperties);
+    const domElement = document.getElementById('tvchart') || '';
+    const chart = createChart(domElement, chartProperties);
     const candleSeries = chart.addCandlestickSeries();
 
 
     this.data$.then(res => res.json())
       .then(data => {
         const cdata = data.map((d: string[]) => {
-          // @ts-ignore
-          return {time:d[0]/1000,open:parseFloat(d[1]),high:parseFloat(d[2]),low:parseFloat(d[3]),close:parseFloat(d[4])}
+          this.price = parseFloat(d[1]);
+          return {
+
+            time: parseFloat(d[0]) / 1000,
+            open: parseFloat(d[1]),
+            high: parseFloat(d[2]),
+            low: parseFloat(d[3]),
+            close: parseFloat(d[4])
+          }
         });
         candleSeries.setData(cdata);
+
       })
       .catch(err => log(err))
+
   }
+
+
 }
