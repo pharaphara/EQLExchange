a4dd6dca3e24b89c23e90a647dd6d79d71c249aa
Msaddek <basem.msaddek@gmail.com>
2022-01-18 13:47:04 +0100

    get wallet & wallet total amount on dashboard

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/front/src/app/dashboard/dashboard.component.ts
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/front/src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {UserService} from "../service/user.service";
 import {User} from "../model/User";
 import {HttpErrorResponse} from "@angular/common/http";
+import {Asset} from "../wallet/state/asset";
+import {AssetService} from "../wallet/service/asset.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -11,13 +13,15 @@ import {HttpErrorResponse} from "@angular/common/http";
 export class DashboardComponent implements OnInit {
 
   user!: User;
+  assets!: Asset[];
+  walletAmount: number = 0;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private assetService: AssetService) {
   }
 
   ngOnInit(): void {
     this.getUser();
-
+    this.getAssets();
   }
 
   public getUser(): void {
@@ -25,13 +29,33 @@ export class DashboardComponent implements OnInit {
       {
         next: (response: User) => {
           this.user = response;
-          console.log(this.user.walletAddress   + 'USERRR')
         },
         error: (error: HttpErrorResponse) => {
           alert(error);
         }
       }
     )
+  }
+
+  public getAssets(): void {
+    this.assetService.getAssets().subscribe({
+        next: (response: Asset[]) => {
+          this.assets = response;
+        },
+        error: (error: HttpErrorResponse) => {
+          alert(error.message);
+        },
+      complete: () => this.getAmountWallet()
+      }
+    );
+  }
+
+  public getAmountWallet(){
+   this.assets?.forEach(
+     element => {
+       this.walletAmount += element.amount
+     },
+   );
   }
 
 }
