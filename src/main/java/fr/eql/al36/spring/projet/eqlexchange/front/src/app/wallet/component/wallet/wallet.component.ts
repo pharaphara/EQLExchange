5d34ac5aff594d6b5d9b051027c2d23f0c1c36cf
Msaddek <basem.msaddek@gmail.com>
2022-01-20 00:05:16 +0100

    Update style

--- src/main/java/fr/eql/al36/spring/projet/eqlexchange/front/src/app/wallet/component/wallet/wallet.component.ts
+++ src/main/java/fr/eql/al36/spring/projet/eqlexchange/front/src/app/wallet/component/wallet/wallet.component.ts
@@ -5,6 +5,8 @@ import {Asset} from '../../state/asset';
 import {UserService} from "../../../service/user.service";
 import {User} from "../../../model/User";
 import {LoginResponse} from "../../../home/model/LogResponse";
+import {Currency} from "../../../explorer/state/currency";
+import {CurrencyService} from "../../../explorer/service/currency.service";
 
 @Component({
   selector: 'app-wallet',
@@ -15,15 +17,18 @@ export class WalletComponent implements OnInit {
 
   public assets!: Asset[];
   public user!: User;
+  currencies!: Currency[];
   amount!: number;
   displayedColumns: string[] = ['Logo', 'Currency', 'Units'];
 
-  constructor(private assetService: AssetService, private userService: UserService) {
+  constructor(private assetService: AssetService, private userService: UserService,
+              private currencyService: CurrencyService) {
   }
 
   public ngOnInit(): void {
     this.getUser();
     this.getAssets();
+    this.getCurrencies();
   }
 
   public getUser(): void {
@@ -59,6 +64,42 @@ export class WalletComponent implements OnInit {
        this.amount = response;
      }
     });
+  }
+
+  public getAssetAmount(ticker: string): number {
+    let result = 0;
+
+    for (let asset of this.assets) {
+      if (asset.currencyTicker == ticker && ticker) {
+        if (ticker === 'EUR') {
+          return asset.amount;
+        }
+        result = asset.amount * this.getCurrencyAmountByTicker(ticker);
+      }
+    }
+    return result;
+  }
+
+  private getCurrencyAmountByTicker(ticker: string): number {
+    let result = 0;
+    for (let currency of this.currencies) {
+      if (currency.ticker == ticker) {
+        result = currency.price;
+      }
+    }
+    return result;
+  }
+
+  public getCurrencies(): void {
+    this.currencyService.getAllCurrencies().subscribe({
+        next: (response: Currency[]) => {
+          this.currencies = response || [];
+        },
+        error: (error: HttpErrorResponse) => {
+          alert(error.message);
+        },
+      }
+    );
   }
 
 }
